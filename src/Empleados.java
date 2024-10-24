3ddda31c0e73c6e55fef4df809d008b4d8ed1370
Ivan-Castill <ivan2003castillo12345@gmail.com>
2024-10-23 23:03:08 -0500

    Tarea semana N#3

--- src/Empleados.java
+++ src/Empleados.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
-
 public class Empleados {
+    private String nombre;
+    private  int edad;
+    private  double sueldo;
+    public Empleados() {
+    }
+    public Empleados(String nombre, int edad, double sueldo) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.sueldo = sueldo;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public double getSueldo() {
+        return sueldo;
+    }
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        Empleados empleado_1 = new Empleados("Norfe",52,408.68);
+        Empleados empleado_2 = new Empleados();
+
+        System.out.println("");
+        System.out.println("------------------------------------------");
+        System.out.println("Bienvenido querido Usuario.");
+        System.out.println("------------------------------------------");
+        System.out.println("");
+        System.out.println("Ingrese el nombre del empleado: ");
+        empleado_2.setNombre(input.nextLine());
+        System.out.println("");
+        System.out.println("Ingrese la edad del empleado: ");
+        empleado_2.setEdad(input.nextInt());
+        input.nextLine();
+        System.out.println("");
+        System.out.println("Ingrese el sueldo del empleado: ");
+        empleado_2.setSueldo(input.nextDouble());
+        System.out.println("------------------------------------------------------");
+        System.out.println("Empledo N_1 Datos quemados:");
+        System.out.println("El nombre es:" + empleado_1.getNombre());
+        System.out.println("La edad que tiene es:" + empleado_1.getEdad()+" Anios");
+        System.out.println("El salario es de:" + empleado_1.getSueldo()+" $");
+        System.out.println("------------------------------------------------------");
+        System.out.println("Empleado N_2 ingresado");
+        System.out.println("El nombre es:" + empleado_2.getNombre());
+        System.out.println("La edad que tiene es:" + empleado_2.getEdad()+" Anios");
+        System.out.println("El salario es de:" + empleado_2.getSueldo()+" $");
+
     }
 }
